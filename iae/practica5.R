845ac27f1709cc541293793976e2e221c8ecc511
Santiago Olszevicki <solszevicki@gmail.com>
2025-04-04 21:26:45 -0300

    end of clase 5

--- iae/practica5.R
+++ iae/practica5.R
@@ -35,3 +35,30 @@ clas_prop_loc(hongos$Height, hongos$Indi, h = 0.1, x_nuevo = 6)
 
 ksmooth(hongos$Height, hongos$Indi, kernel = "box", bandwidth = 0.2, x.points = c(5.2,6))
 
+
+# EJERCICIO 6: METODO GENERATIVO
+
+f0 <- density(hongos$Height[hongos$Indi == 0], kernel = "gaussian", bw = bw.bcv(hongos$Height[hongos$Indi == 0]))
+f1 <- density(hongos$Height[hongos$Indi == 1], kernel = "gaussian", bw = bw.bcv(hongos$Height[hongos$Indi == 1]))
+
+hist(hongos$Height[hongos$Indi == 0], freq = F, col = "lightgreen")
+lines(f0, col = "darkgreen")
+hist(hongos$Height[hongos$Indi == 1], freq = F, col = "pink", add = T)
+
+lines(f1, col = "red")
+
+
+p1 <- mean(hongos$Indi)
+p0 <- 1-p1
+
+f0.aprox <- approxfun(f0) # Tiene que haber una manera de ver la densidad exacta
+f1.aprox <- approxfun(f1)
+
+x_nuevo <- 6
+f1.aprox(x_nuevo)*p1 > f0.aprox(x_nuevo)*p1
+
+
+bayes_cutoff_p0.5 <- uniroot(function(x){f0.aprox(x) - f1.aprox(x)}, lower = 5.2, upper = 6)$root
+bayes_cutoff_p.real <- uniroot(function(x){p0*f0.aprox(x) - p1*f1.aprox(x)}, lower = 5.2, upper = 6)$root
+abline(v = bayes_cutoff_p.real, col = "darkgreen", lty = 2, lwd = 2)
+abline(v = bayes_cutoff, col = "black", lty = 1, lwd = 0.5)
